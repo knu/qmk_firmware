80b5b4a9763bfe497c544c896530cc61138feaac
Akinori MUSHA <knu@idaemons.org>
2021-03-24 00:03:33 +0900

    Use enum

--- keyboards/handwired/dactyl_manuform/6x6/keymaps/knu/keymap.c
+++ keyboards/handwired/dactyl_manuform/6x6/keymaps/knu/keymap.c
@@ -1,12 +1,10 @@
 #include QMK_KEYBOARD_H
 
-
-#define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
-
-#define RAISE MO(_RAISE)
-#define LOWER MO(_LOWER)
+enum layers {
+  QWERTY,
+  LOWER,
+  RAISE,
+};
 
 #define LSFT_GRAVE	MT(MOD_LSFT,KC_GRAVE)
 #define RSFT_BSLS	MT(MOD_RSFT,KC_BSLS)
@@ -16,7 +14,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_QWERTY] = LAYOUT_6x6(
+  [QWERTY] = LAYOUT_6x6(
 
     KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,             KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12 ,
     KC_ESC,    KC_1,      KC_2,      KC_3,      KC_4,      KC_5,              KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      KC_BSPC,
@@ -26,10 +24,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           KC_EQL,    KC_LBRC,                                                       KC_GRAVE,  KC_RBRC,
                                                 KC_SPC,    LGUI_EISUU,        KC_ENT,    KC_BSPC,
                                                 LALT_DEL,  KC_HOME,           KC_PGUP,   RALT_KANA,
-                                                LOWER,     KC_END,            KC_PGDN,   RAISE
+                                                TT(LOWER), KC_END,            KC_PGDN,   TT(RAISE)
   ),
 
-  [_LOWER] = LAYOUT_6x6(
+  [LOWER] = LAYOUT_6x6(
 
     KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,             KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12 ,
     _______,   _______,   _______,   _______,   _______,   _______,           _______,   _______,   KC_LPRN,   KC_RPRN,   KC_SLSH,   KC_BSPC,
@@ -42,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                 _______,   _______,           _______,   _______
   ),
 
-  [_RAISE] = LAYOUT_6x6(
+  [RAISE] = LAYOUT_6x6(
 
     KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,             KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12 ,
     _______,   _______,   _______,   _______,   _______,   KC_LBRC,           KC_RBRC,   _______,   KC_NLCK,   KC_INS,    KC_SLCK,   KC_MUTE,
