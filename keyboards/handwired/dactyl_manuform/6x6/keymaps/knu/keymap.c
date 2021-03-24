4663a411865444f679b146f11e3b7e7f3e3c3a13
Akinori MUSHA <knu@idaemons.org>
2021-03-24 09:52:30 +0900

    Place ]/- instead of `/]
    - This makes [ and ] symmetric.
    - ` is redundant when it is accessible with the left pinky.
    - Making - more accessible helps.

--- keyboards/handwired/dactyl_manuform/6x6/keymaps/knu/keymap.c
+++ keyboards/handwired/dactyl_manuform/6x6/keymaps/knu/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,              KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      KC_MINS,
     KC_LCTL,   KC_A,      KC_S,      KC_D,      KC_F,      KC_G,              KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,   KC_QUOT,
     LSFT_GRAVE,KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,              KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,   RSFT_BSLS,
-                          KC_EQL,    KC_LBRC,                                                       KC_GRAVE,  KC_RBRC,
+                          KC_EQL,    KC_LBRC,                                                       KC_RBRC,   KC_MINS,
                                                 KC_SPC,    LGUI_EISUU,        RAISE_ENT, KC_BSPC,
                                                 LALT_DEL,  KC_HOME,           KC_PGUP,   RALT_KANA,
                                                 TT(LOWER), KC_END,            KC_PGDN,   TT(LOWER)
